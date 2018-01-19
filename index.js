f4c317da1bb123779769bee281cb5f340d99d3d4
Asif Ahmed <asif.h.ahmed@gmail.com>
2018-01-19 10:03:08 -0800

    Coins

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
-var ccxt = require('ccxt')
+const request = require('request-promise')
 
-let runner = async function() {
-  const cmc = new ccxt.coinmarketcap()
-    
-  const markets = await cmc.loadMarkets()
+var twilio = require('twilio')
+var accountSid = process.env.TWILIO_SID
+var authToken = process.env.TWILIO_TOKEN
 
-  let pairings = Object.keys(markets)
+var client = new twilio(accountSid, authToken)
 
-  const coins = [
-    ['BTC', 1],
-    ['LTC', 100],
-    ['ETH', 10],
-  ]
-
-  let sum = 0
-
-  coins.forEach(function(x){
-    let coin = x[0]
-    let amount = x[1]
-    let info = markets[coin + '/USD'] 
-    let price = info.info.price_usd
-    let total = price * amount
-    console.log(coin, '|', price, '|', amount, '|', total.toFixed(2))
-    sum += total
+let getTotalPortfolioPrice = async function() {
+  const cmc = await request({
+    url: `https://api.coinmarketcap.com/v1/ticker/?limit=0`,
+    json: true,
   })
 
-  console.log("Total", sum.toFixed(2))
+  const coins = JSON.parse(process.env.CRYPTO_HOLDINGS)
 
-  // console.log (ccxt.exchanges) // print all available exchangesZ
+  cmc.forEach(function(x){
+    const symbol = x.symbol
+    if(coins[symbol] && !coins[symbol].info){
+      coins[symbol].info = x
+    }
+  })
+  
+  let sum = 0
+  for(coin in coins){
+    const amount = coins[coin].amount
+    const price = coins[coin].info.price_usd
+    const total = price * amount
+    // console.log(coin, '|', price, '|', amount, '|', total.toFixed(2))
+    sum += total
+  }
+
+  return sum
 };
 
-runner()
+let profitLossPercentage = async function() {
+  let initialInvestment = process.env.CRYPTO_INITIAL_INVESTMENT
+  let total = await getTotalPortfolioPrice()
+
+  return (total/initialInvestment) * 100.0
+}
+
+let messageUser = async function(){
+  const profitLoss = await profitLossPercentage()
+  console.log(profitLoss)
+  client.messages.create({
+    body: profitLoss.toFixed(),
+    to: process.env.TWILIO_TO_NUMBER,
+    from: process.env.TWILIO_FROM_NUMBER
+  })
+  .then((message) => console.log(message.sid))
+  .catch((error) => console.log(error))
+};
+
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const interval = hour * 24
+
+messageUser()
+
+setInterval(function () { 
+  messageUser()
+}, interval);
+
+require('http').createServer((req, res) => {
+  res.end('▲ Hello World')
+}).listen(process.env.PORT)
+
+
+
+
+
 
